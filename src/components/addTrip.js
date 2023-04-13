b17406968f389aa6e38af52720485d43ddd0114e
Nico Paredes <pared055@umn.edu>
2023-04-13 18:25:42 -0500

    installed react-calendar to allow users to select dates from small calendar popup, will integrate with component soon

--- src/components/addTrip.js
+++ src/components/addTrip.js
@@ -1,12 +1,44 @@
 import React, { useState, useEffect } from "react";
 
 export default function AddTrip( {group}) {
-    // const [loading, setLoading] = useState(true);
-    // const { isLoaded, userId, sessionId, getToken } = useAuth();
+    const [tripName, setTripName] = useState("");
+  const [startDate, setStartDate] = useState("");
+  const [endDate, setEndDate] = useState("");
+  const [description, setDescription] = useState("");
 
     return (
         <div id="add-trip-container">
             <h1>Create Trip</h1>
+            <div>
+                <input 
+                    placeholder="Trip Name"
+                    value={tripName}
+                    onChange={(e) => setTripName(e.target.value)}
+                ></input>
+            </div>
+            <div>
+                <input 
+                    placeholder="Start Date"
+                    value={startDate}
+                    onChange={(e) => setStartDate(e.target.value)}
+                ></input>
+            </div>
+            <div>
+                <input 
+                    placeholder="End Date"
+                    value={endDate}
+                    onChange={(e) => setEndDate(e.target.value)}
+                ></input>
+            </div>
+            <div>
+                <input 
+                    placeholder="Brief Description"
+                    value={description}
+                    onChange={(e) => setDescription(e.target.value)}
+                ></input>
+            </div>
+
+            <button id="add-trip">Add Trip</button>
         </div>
       );
-  }
+}
