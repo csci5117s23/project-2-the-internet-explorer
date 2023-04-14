1adaae754f502782115879f3e2398b3295a65f4a
Zchamp21 <zchamp1000@gmail.com>
2023-04-13 20:03:26 -0500

    Added a header bar component

--- src/components/Header.js
+++ src/components/Header.js
@@ -4,11 +4,13 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import styles from '../styles/Trips.module.css';
 
 export default function Header() {
+  // TODO: Update this so that instead of saying 'Your Trips', it takes in a prop to indicate
+  // TODO: what the "header message" should say.
+
   return (
-    <div className='grid grid-cols-6 gap-4'>
-      <h1 className={styles.headerBar}><FontAwesomeIcon icon={faBars} /></h1>
-      <h1 className={`${styles.headerBar} col-span-4`}>Your Trips</h1>
-      <h1 className={styles.headerBar}><UserButton></UserButton></h1>
+    <div className={`${styles.headerDiv} grid grid-cols-6 gap-4`}>
+      <h1 className={`${styles.headerTitle} col-span-5`}>Your Trips</h1>
+      <h1><UserButton /></h1>
     </div>
-  )
+  );
 }
