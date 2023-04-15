d4386b814f37fbfd2dd7c77e1b5f3780e4b28517
Nat0110tan <tan00250@umn.edu>
2023-04-15 12:43:09 -0500

    added links to a trip

--- trip/index.js
+++ trip/index.js
@@ -3,16 +3,26 @@
 * Auto generated Codehooks (c) example
 * Install: npm i codehooks-js codehooks-crudlify
 */
-import {app} from 'codehooks-js'
-import {crudlify} from 'codehooks-crudlify'
+import {app, Datastore} from 'codehooks-js';
+import {crudlify} from 'codehooks-crudlify';
+import jwtDecode from "jwt-decode";
+import { object, string } from "yup";
 
 // test route for https://<PROJECTID>.api.codehooks.io/dev/
 app.get('/', (req, res) => {
   res.send('CRUD server ready')
 })
 
+//create the trip folder
+const tripFolderYup = object({
+  tripName: string().required(),
+  startDate: string().required(),
+  endDate: string().required(),
+  description: string(),
+});
+
 // Use Crudlify to create a REST API for any collection
-crudlify(app)
+crudlify(app, {tripFolder: tripFolderYup})
 
 // bind to serverless runtime
 export default app.init();
