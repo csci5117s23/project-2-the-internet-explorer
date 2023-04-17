649bcafebdbbd2b82e95d00e6951e588b206afcd
Nico Paredes <pared055@umn.edu>
2023-04-16 19:40:26 -0500

    got rid of react-calendar

--- src/components/AddTrip.js
+++ src/components/AddTrip.js
@@ -1,64 +1,11 @@
-import React, { useState, useEffect, useRef } from "react";
+import React, { useState } from "react";
 import styles from '../styles/AddTrip.module.css';
-import Calendar from 'react-calendar'
-import 'react-calendar/dist/Calendar.css';
 
 export default function AddTrip() {
     const [tripName, setTripName] = useState("");
     const [startDate, setStartDate] = useState("");
     const [endDate, setEndDate] = useState("");
     const [description, setDescription] = useState("");
-    const [endDateError, setEndDateError] = useState("");
-    const [showStartDateCalendar, setShowStartDateCalendar] = useState(false);
-    const [showEndDateCalendar, setShowEndDateCalendar] = useState(false);
-
-    const startDateInputRef = useRef();
-    const startDateCalendarRef = useRef();
-    const endDateInputRef = useRef();
-    const endDateCalendarRef = useRef();
-    
-    const handleStartDateChange = (date) => {
-        // Format the selected date as a string (e.g., "YYYY-MM-DD")
-        const formattedDate = date.toISOString().split("T")[0];
-        setStartDate(formattedDate);
-    };
-
-    const handleEndDateChange = (date) => {
-        // Format the selected date as a string (e.g., "YYYY-MM-DD")
-        const formattedDate = date.toISOString().split("T")[0];
-        setEndDate(formattedDate);
-
-        // Check if the end date is before the start date
-        if (new Date(formattedDate) < new Date(startDate)) {
-            setEndDateError("End date cannot be before the start date.");
-        } else {
-            setEndDateError(""); // Clear the error message if the end date is valid
-        }
-    };
-
-    const handleClickOutside = (e) => {
-        if (
-          !startDateInputRef.current.contains(e.target) &&
-          !startDateCalendarRef.current?.contains(e.target)
-        ) {
-          setShowStartDateCalendar(false);
-        }
-
-        if (
-          !endDateInputRef.current.contains(e.target) &&
-          !endDateCalendarRef.current?.contains(e.target)
-        ) {
-          setShowEndDateCalendar(false);
-        }
-    };
-
-    useEffect(() => {
-        document.addEventListener("mousedown", handleClickOutside);
-        return () => {
-            document.removeEventListener("mousedown", handleClickOutside);
-        };
-    }, []);
-    
 
     return (
         <>
@@ -76,35 +23,22 @@ export default function AddTrip() {
             <div className="p-4 start-date-container">
                 <h4 className="text-l font-bold" id="createTripHeader">Start Date</h4>
                 <input
-                    ref={startDateInputRef} 
+                    type="date"
                     className="border-2 border-slate-600 w-full"
                     placeholder="Start Date"
                     value={startDate}
                     onChange={(e) => setStartDate(e.target.value)}
-                    onFocus={() => setShowStartDateCalendar(true)}
                 ></input>
-                {showStartDateCalendar && (
-                    <div className= "calendarDiv" ref={startDateCalendarRef}>
-                        <Calendar onChange={handleStartDateChange} />
-                    </div>
-                )}
             </div>
             <div className="p-4 end-date-container">
                 <h4 className="text-l font-bold" id="createTripHeader">End Date</h4>
                 <input 
-                    ref={endDateInputRef}
+                    type="date"
                     className="border-2 border-slate-600 w-full"
                     placeholder="End Date"
                     value={endDate}
                     onChange={(e) => setEndDate(e.target.value)}
-                    onFocus={() => setShowEndDateCalendar(true)} 
                 ></input>
-                {endDateError && <p className="text-red-500">{endDateError}</p>}
-                {showEndDateCalendar && (
-                    <div className= "calendarDiv" ref={endDateCalendarRef}>
-                        <Calendar ref={endDateCalendarRef} onChange={handleEndDateChange} />
-                    </div>
-                )}
             </div>
             <div className="p-4">
                 <h4 className="text-l font-bold" id="createTripHeader">Brief Description</h4>
