c3abf8f214208b319cb867697b73484e016a35a1
Ryan <exner014@umn.edu>
2023-04-17 10:51:10 -0500

    day view

--- src/components/buttons/DayViewButton.js
+++ src/components/buttons/DayViewButton.js
@@ -1,9 +1,9 @@
 
-const DayViewButton = ({title}) => {
+const DayViewButton = ({title, color}) => {
     return (
         <button
             type="button"
-            class="inline-block rounded px-14 pt-28 pb-4 text-s font-medium uppercase leading-normal text-neutral-50 -[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out bg-custom-blue hover:bg-black"
+            class={"inline-block rounded px-14 pt-28 pb-4 text-s font-medium uppercase leading-normal text-neutral-50 -[0_4px_9px_-4px_rgba(51,45,45,0.7)] transition duration-150 ease-in-out hover:bg-black " + color}
             >
             {title}
         </button>
