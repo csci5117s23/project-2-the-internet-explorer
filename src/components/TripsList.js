953330b2ad9366c6f290a3dcad04836d95d2de2b
Zchamp21 <zchamp1000@gmail.com>
2023-04-17 11:22:04 -0500

    Added the link to individual trip view

--- src/components/TripsList.js
+++ src/components/TripsList.js
@@ -1,12 +1,14 @@
 import Trip from "./Trip";
 import styles from '../styles/Trips.module.css';
+import { tileProps } from "react-calendar/dist/cjs/shared/propTypes";
 
 export default function TripsList({ trips }) {
   // This is the component for the list of trips.
   let tripsList = trips.map(
     (trip, i) => (
       <Trip
-        key={trip.tripName}
+        key={trip._id}
+        id={trip._id}
         title={trip.tripName}
         startDate={trip.startDate}
         endDate={trip.endDate}
