a668c14d40e892e2b80b6e62e1214e0e284cee00
Nico Paredes <pared055@umn.edu>
2023-04-18 17:09:13 -0500

    changed words for choosing type of memory

--- src/components/TripMemory.js
+++ src/components/TripMemory.js
@@ -43,7 +43,7 @@ export default function TripMemory() {
             </div>
             <Map></Map>
             <div className="p-4">
-                <h4 className="text-l font-bold">What kind of memory is this?</h4>
+                <h4 className="text-l font-bold">What type of memory is this?</h4>
                 <select 
                     className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50" 
                     name="folders" 
