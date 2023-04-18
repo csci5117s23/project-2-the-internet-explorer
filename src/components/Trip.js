838e64eac8b1f02ab081312656dcebdf7f99c03a
Ryan <exner014@umn.edu>
2023-04-18 15:40:13 -0500

    make whole trip clickable instead of just name

--- src/components/Trip.js
+++ src/components/Trip.js
@@ -27,14 +27,15 @@ export default function Trip({ id, title, startDate, endDate, index }) {
 
   // TODO make edit/delete buttons little circles w/ icons inside that only appear on hover
   return (
+    <>
+    <Link href={`/trips/${id}`}>
     <div className={styles.tripDiv} style={{backgroundColor: colors[index]}}>
-
-
-      
       {/* <div className={styles.editDelDiv}><span className={styles.editDelSpan}><span>Edit</span> | <span>Delete</span></span></div><br></br> */}
       <div className={styles.dateDiv}><span>{date_format[0] + " " + date_format[1]}</span></div><br></br>
-      <div className={styles.title}><Link href={`/trips/${id}`}><span>{title}</span></Link></div>
+      <div className={styles.title}><span>{title}</span></div>
    
     </div>
+    </Link>
+    </>
   );
 }
