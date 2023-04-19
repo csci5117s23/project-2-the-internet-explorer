9c56f0ebd5525b79d9fffeef706540a3e7b2c385
Zchamp21 <zchamp1000@gmail.com>
2023-04-19 10:19:31 -0500

    Removed some commented out stuff

--- src/components/Map.js
+++ src/components/Map.js
@@ -62,119 +62,39 @@ export default function Map({ location, setLocation, coordinates, setCoordinates
               readOnly
             ></input>
           </div>
-          {/* <LoadScript
-            libraries={libraries}
-            googleMapsApiKey={MAP_API}
-          > */}
-            <GoogleMap
-              id="searchbox-example"
-              mapContainerStyle={mapContainerStyle}
-              zoom={12}
-              center={userPosition}
-              onLoad={setMapInstance}
+          <GoogleMap
+            id="searchbox-example"
+            mapContainerStyle={mapContainerStyle}
+            zoom={12}
+            center={userPosition}
+            onLoad={setMapInstance}
+          >
+            {coordinates && <Marker position={coordinates} />}
+            <StandaloneSearchBox
+              onLoad={onLoad}
+              onPlacesChanged={onPlacesChanged}
             >
-              {coordinates && <Marker position={coordinates} />}
-              <StandaloneSearchBox
-                onLoad={onLoad}
-                onPlacesChanged={onPlacesChanged}
-              >
-                <input
-                  type="text"
-                  placeholder="Begin typing location..."
-                  style={{
-                    boxSizing: `border-box`,
-                    border: `1px solid transparent`,
-                    width: `240px`,
-                    height: `32px`,
-                    padding: `0 12px`,
-                    borderRadius: `3px`,
-                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                    fontSize: `14px`,
-                    outline: `none`,
-                    textOverflow: `ellipses`,
-                    position: "absolute",
-                    left: "50%",
-                    marginLeft: "-120px",
-                  }}
-                />
-              </StandaloneSearchBox>
-            </GoogleMap>
-          {/* </LoadScript> */}
-          {/* {window.google === undefined ? (
-            <LoadScript
-              libraries={libraries}
-              googleMapsApiKey={MAP_API}
-            >
-              <GoogleMap
-                id="searchbox-example"
-                mapContainerStyle={mapContainerStyle}
-                zoom={12}
-                center={userPosition}
-                onLoad={setMapInstance}
-              >
-                {coordinates && <Marker position={coordinates} />}
-                <StandaloneSearchBox
-                  onLoad={onLoad}
-                  onPlacesChanged={onPlacesChanged}
-                >
-                  <input
-                    type="text"
-                    placeholder="Begin typing location..."
-                    style={{
-                      boxSizing: `border-box`,
-                      border: `1px solid transparent`,
-                      width: `240px`,
-                      height: `32px`,
-                      padding: `0 12px`,
-                      borderRadius: `3px`,
-                      boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                      fontSize: `14px`,
-                      outline: `none`,
-                      textOverflow: `ellipses`,
-                      position: "absolute",
-                      left: "50%",
-                      marginLeft: "-120px",
-                    }}
-                  />
-                </StandaloneSearchBox>
-              </GoogleMap>
-            </LoadScript>
-          ) : (
-            <GoogleMap
-              id="searchbox-example"
-              mapContainerStyle={mapContainerStyle}
-              zoom={12}
-              center={userPosition}
-              onLoad={setMapInstance}
-            >
-              {coordinates && <Marker position={coordinates} />}
-              <StandaloneSearchBox
-                onLoad={onLoad}
-                onPlacesChanged={onPlacesChanged}
-              >
-                <input
-                  type="text"
-                  placeholder="Begin typing location..."
-                  style={{
-                    boxSizing: `border-box`,
-                    border: `1px solid transparent`,
-                    width: `240px`,
-                    height: `32px`,
-                    padding: `0 12px`,
-                    borderRadius: `3px`,
-                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                    fontSize: `14px`,
-                    outline: `none`,
-                    textOverflow: `ellipses`,
-                    position: "absolute",
-                    left: "50%",
-                    marginLeft: "-120px",
-                  }}
-                />
-              </StandaloneSearchBox>
-            </GoogleMap>
-          )} */}
-          
+              <input
+                type="text"
+                placeholder="Begin typing location..."
+                style={{
+                  boxSizing: `border-box`,
+                  border: `1px solid transparent`,
+                  width: `240px`,
+                  height: `32px`,
+                  padding: `0 12px`,
+                  borderRadius: `3px`,
+                  boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                  fontSize: `14px`,
+                  outline: `none`,
+                  textOverflow: `ellipses`,
+                  position: "absolute",
+                  left: "50%",
+                  marginLeft: "-120px",
+                }}
+              />
+            </StandaloneSearchBox>
+          </GoogleMap>
         </div>
       </>
     );
