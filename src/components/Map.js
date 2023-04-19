837125785af68a93175e72ee013fa91eea0fa413
Nat0110tan <tan00250@umn.edu>
2023-04-19 11:30:24 -0500

    finished individual memory page

--- src/components/Map.js
+++ src/components/Map.js
@@ -53,59 +53,54 @@ export default function Map() {
     return (
       <>
         <div className="p-4">
-                <h4 className="text-l font-bold">Location</h4>
-                <div>
+          <h4 className="text-l font-bold">Location</h4>
+          <div>
+            <input
+              className="border-2 border-slate-600 mb-6 w-full"
+              placeholder="Location"
+              value={location}
+              readOnly
+            ></input>
+          </div>
+          <LoadScript
+            libraries={libraries}
+            googleMapsApiKey={MAP_API}
+          >
+            <GoogleMap
+              id="searchbox-example"
+              mapContainerStyle={mapContainerStyle}
+              zoom={12}
+              center={userPosition}
+              onLoad={setMapInstance}
+            >
+              {coordinates && <Marker position={coordinates} />}
+              <StandaloneSearchBox
+                onLoad={onLoad}
+                onPlacesChanged={onPlacesChanged}
+              >
                 <input
-                    className="border-2 border-slate-600 mb-6 w-full"
-                    placeholder="Location"
-                    value={location}
-                    readOnly
-                ></input>
-                </div>
-                <LoadScript
-                    libraries={libraries}
-                >
-                <GoogleMap
-                    id="searchbox-example"
-                    mapContainerStyle={mapContainerStyle}
-                    zoom={12}
-                    center={userPosition}
-                    onLoad={setMapInstance}
-                >
-                    {coordinates && (
-                     <Marker
-                        position={coordinates}
-                        />
-                    )}
-                    <StandaloneSearchBox
-                    onLoad={onLoad}
-                    onPlacesChanged={
-                        onPlacesChanged
-                    }
-                    >
-                    <input
-                        type="text"
-                        placeholder="Begin typing location..."
-                        style={{
-                        boxSizing: `border-box`,
-                        border: `1px solid transparent`,
-                        width: `240px`,
-                        height: `32px`,
-                        padding: `0 12px`,
-                        borderRadius: `3px`,
-                        boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
-                        fontSize: `14px`,
-                        outline: `none`,
-                        textOverflow: `ellipses`,
-                        position: "absolute",
-                        left: "50%",
-                        marginLeft: "-120px"
-                        }}
-                    />
-                    </StandaloneSearchBox>
-                </GoogleMap>
-                </LoadScript>
-            </div>
-        </>
-    )
+                  type="text"
+                  placeholder="Begin typing location..."
+                  style={{
+                    boxSizing: `border-box`,
+                    border: `1px solid transparent`,
+                    width: `240px`,
+                    height: `32px`,
+                    padding: `0 12px`,
+                    borderRadius: `3px`,
+                    boxShadow: `0 2px 6px rgba(0, 0, 0, 0.3)`,
+                    fontSize: `14px`,
+                    outline: `none`,
+                    textOverflow: `ellipses`,
+                    position: "absolute",
+                    left: "50%",
+                    marginLeft: "-120px",
+                  }}
+                />
+              </StandaloneSearchBox>
+            </GoogleMap>
+          </LoadScript>
+        </div>
+      </>
+    );
 }
--- src/pages/trips/[tripID]/[memoryID].js
+++ src/pages/trips/[tripID]/[memoryID].js
@@ -1,6 +1,120 @@
+const backend_base = process.env.NEXT_PUBLIC_BACKEND_BASE_URL;
+const MAP_API = process.env.NEXT_PUBLIC_MAP_API;
+import { useEffect, useState } from "react";
+import { useAuth } from "@clerk/nextjs";
 import { useRouter } from "next/router";
-export default function MemoryView() {
+import Header from "@/components/Header";
+import TripMemoryWrapper from "@/components/TripMemoryWrapper";
+import DayViewButton from "@/components/buttons/DayViewButton";
+import Link from "next/link";
+import { GoogleMap, LoadScript, Marker } from "@react-google-maps/api";
+
+export default function MemoryView({}) {
   const router = useRouter();
-  const memoryID = router.query.memoryID;
-  return <div>{memoryID}</div>;
+  const { isLoaded, userId, sessionId, getToken } = useAuth();
+  const [isLoading, setIsLoading] = useState(false);
+  // const memoryId = router.query.memoryID;
+  const [center, setCenter] = useState({ lat: 44.9718, lng: -93.2338 });
+  // const tripId = router.query.tripID;
+
+  const [memory, setMemory] = useState(null);
+//get the memory from database
+  // useEffect(() => {
+  //   const getIndividualMemory = async () => {
+  //     if (router.isReady) {
+  //       const memoryId = router.query.memoryID;
+  //       const tripId = router.query.tripID;
+  //       try {
+  //         if (userId) {
+  //           const token = await getToken({ template: "codehooks" });
+  //           const response = await fetch(
+  //             backend_base + `/tripMemories/${tripId}/${memoryId}`,
+  //             {
+  //               method: "GET",
+  //               headers: {
+  //                 Authorization: "Bearer " + token,
+  //               },
+  //             }
+  //           );
+  //           if (!response.ok) {
+  //             router.push("/404");
+  //             return;
+  //           }
+  //           const data = await response.json();
+  //           setMemory(data);
+  //           setIsLoading(false);
+  //         }
+  //       } catch (error) {
+  //         console.error("Error: ", error);
+  //       }
+  //     }
+  //   };
+  //   getIndividualMemory();
+  // }, [isLoaded, router.isReady]);
+
+  // return <div>[memoryID]: {memoryId} [parentTripId]: {tripId}</div>;
+
+  // useEffect(() => {
+  //   // get the location from the database
+  //   const userLocation = // user location
+  //     // set the center
+  //     setCenter({
+  //       lat: userLocation.latitude,
+  //       lng: userLocation.longitude,
+  //     });
+  // }, []);
+
+
+  //   parentTripId: string().required(),  // The id of the parent trip
+  // title: string().required(),         // The title of the memory
+  // description: string(),              // An optional description of the memory.
+  // date: date().required,              // The date of the memory.
+  // address: string().required,         // The address/location of the memory.
+  // category: string().required,        // The category of the memory.
+  // image: string().required,           // An image of the memory.
+  // user: string().required,  
+
+  return isLoading ? (
+    <h1>LOADING Memory...</h1>
+  ) : (
+    <>
+      <Header title={"memory title"} />
+      <div className="grid gap-1 place-items-center m-8">
+        <div style={{ width: "400px" }} className="p-2">
+          <img src="https://project50017.s3.us-east-2.amazonaws.com/winter.jpeg" />
+        </div>
+        {/* <DayViewButton title={"June 1"} color={"bg-custom-blue"} /> */}
+        <div className="flex p-2">
+          <div className="rounded-lg bg-blue-400 text-white p-2 mr-2">
+            <Link href="/">
+              {/* {date} */}
+              June 1
+            </Link>
+          </div>
+          <div className="rounded-lg bg-sky-400 text-white p-2 mr-2">
+            <Link href="/">places</Link>
+          </div>
+          <div className="rounded-lg bg-cyan-400 text-white p-2">
+            <Link href="/">user</Link>
+          </div>
+        </div>
+        <div>
+          <LoadScript googleMapsApiKey={MAP_API}>
+            <GoogleMap
+              id="example-map"
+              center={center}
+              zoom={10}
+              mapContainerStyle={{ height: "400px", width: "400px" }}
+            >
+              {center && <Marker position={center} />}
+            </GoogleMap>
+          </LoadScript>
+        </div>
+        <div>description:</div>
+        <div className="flex justify-center bg-blue-100 border-4 border-blue-300 rounded-lg shadow-sm p-4 mb-4">
+          MN: wdym summer?
+        </div>
+      </div>
+    </>
+  );
 }
