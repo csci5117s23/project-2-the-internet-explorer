c65dc8ba2df2f9f27646cf9a40bebc3a7177f956
Zchamp21 <zchamp1000@gmail.com>
2023-04-24 23:06:53 -0500

    Fixed the add memory again, I hope

--- src/components/TripMemory.js
+++ src/components/TripMemory.js
@@ -116,7 +116,7 @@ export default function TripMemory({ addMemory, closeModal, parentId, setDataUrl
             category: formJson.folders,
         }
 
-        addMemory(newMemory);
+        // addMemory(newMemory);
         e.target.reset();
         closeModal();
     }
@@ -233,7 +233,7 @@ export default function TripMemory({ addMemory, closeModal, parentId, setDataUrl
                 ></Map>
                 <div className="p-4">
                     <h4 className="text-l font-bold">What type of memory is this?</h4>
-                    {category !== undefined ? (
+                    {category ? (
                         <select 
                             className="inline-flex w-full justify-center gap-x-1.5 rounded-md bg-white px-3 py-2 text-sm font-semibold text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 hover:bg-gray-50"
                             name="folders"
