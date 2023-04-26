a6c6bcf6b590661a1d2ed017ca2a0cafffe0a718
Zchamp21 <zchamp1000@gmail.com>
2023-04-26 15:48:04 -0500

    Fixed the fontawesome icons i hope

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,6 +1,9 @@
 import '@/styles/globals.css'
 import { ClerkProvider } from '@clerk/nextjs'
 import Head from "next/head"
+import "@fortawesome/fontawesome-svg-core/styles.css";
+import { config } from "@fortawesome/fontawesome-svg-core";
+config.autoAddCss = false;
 
 export default function App({ Component, pageProps }) {
   return (
