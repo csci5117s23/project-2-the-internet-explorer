78d176e323176bd1215916a7a611ee17e45f9880
Nico Paredes <pared055@umn.edu>
2023-04-27 11:10:59 -0500

    fixed styling for header

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,11 +7,11 @@ import DayDropdown from "./buttons/DayDropdown";
 import Link from "next/link";
 export default function Header({title, back, prevUrl, day}) {
   return (
-    <div className={`${styles.headerDiv} grid grid-cols-12`}>
-      <div className="col-start-1 col-span-1 flex items-center pl-3 gap-3">
+    <div className={`${styles.headerDiv} flex w-full`}>
+      <div className="flex items-center pl-3 gap-3">
         {back ? <BackButton prevUrl={prevUrl} /> : <div></div>}
       </div>
-      <div className="col-start-2 col-span-2 flex items-center gap-3 justify-center">
+      <div className="flex items-center ml-4">
         <Link href="/trips">
           <FontAwesomeIcon
             icon={faHouse}
@@ -19,11 +19,11 @@ export default function Header({title, back, prevUrl, day}) {
           />
         </Link>
       </div>
-      <div className="col-start-4 col-end-10 flex justify-center py-1 ">
+      <div className="flex justify-center flex-grow py-1 ">
         <h1 className={`${styles.headerTitle}`}>{title}</h1>
       </div>
       {/* <div className="col-start-9 col-end-10"></div> */}
-      <div class="flex col-start-10 col-span-3 pr-2 justify-center items-center w-full h-full pl-1">
+      <div class="flex justify-end items-center pr-2">
         <div class=" justify-center ml-4">
           <UserButton appearance={{ elements: { rootBox: "scale-150" } }} />
         </div>
