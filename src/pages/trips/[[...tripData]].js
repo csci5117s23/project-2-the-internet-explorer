b9b679c28a8395813e8f26359b6a6c7f213994f2
Zchamp21 <zchamp1000@gmail.com>
2023-04-29 14:30:48 -0500

    Added a comment

--- src/pages/trips/[[...tripData]].js
+++ src/pages/trips/[[...tripData]].js
@@ -8,6 +8,8 @@ import IndividualWrapper from "@/components/IndividualWrapper";
 export default function TripData() {
   const router = useRouter();
 
+  // Wait for the router to be ready before loading any components. We need the router ready
+  // to ensure we can retrieve the query from the router.
   if (router.isReady) {
     return (
       <IndividualWrapper router={router}></IndividualWrapper>
