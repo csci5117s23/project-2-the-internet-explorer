7ccbe0442345915825ffca9c294ec7028c784f1d
Zchamp21 <zchamp1000@gmail.com>
2023-05-01 20:22:21 -0500

    Finished the last of the code style changes

--- src/components/buttons/BackButton.js
+++ src/components/buttons/BackButton.js
@@ -2,10 +2,9 @@ import React from "react";
 import Link from "next/link";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faChevronLeft } from "@fortawesome/free-solid-svg-icons";
-import styles from "../../styles/Trips.module.css";
 
 export default function BackButton({ prevUrl }) {
   return (
-    <Link className={styles.backButton} href={prevUrl}><FontAwesomeIcon className="text-4xl text-white" icon={faChevronLeft}/></Link>
+    <Link href={prevUrl}><FontAwesomeIcon className="text-4xl text-white" icon={faChevronLeft}/></Link>
   );
 };
